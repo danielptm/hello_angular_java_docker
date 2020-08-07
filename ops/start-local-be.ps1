8fd4e08c06ceeae105e1d23e09b6882574016796
loganballard <loganballard@gmail.com>
2020-08-07 11:39:40 -0700

    Added service 2

--- ops/start-local-be.ps1
+++ ops/start-local-be.ps1
@@ -5,16 +5,21 @@ $ErrorActionPreference='Stop';
 Set-Variable -name VERSION -Value 0.0.2;
 Set-Variable -name CURDIR -Value $(Get-Location);
 
-# Build Docker Image
+# Build Docker Images
 & minikube -p minikube docker-env | Invoke-Expression;  # build the docker image on minikube, not local machine
 Set-Location $CURDIR\HelloStandardBE;
 mvn clean install;
 docker build . -t hello-standard-be:$VERSION;
+Set-Location $CURDIR\HelloStandardBE2;
+mvn clean install;
+docker build . -t hello-standard-be-2:$VERSION;
 Remove-Item -Path Env:DOCKER*;  # set docker-engine context back to local machine
 
 # Start Kubernetes Resources
 kubectl apply -f $CURDIR\ops\k8s\local\backend-deploy.yaml
 kubectl apply -f $CURDIR\ops\k8s\local\backend-service.yaml
+kubectl apply -f $CURDIR\ops\k8s\local\backend-deploy-2.yaml
+kubectl apply -f $CURDIR\ops\k8s\local\backend-service-2.yaml
 kubectl apply -f $CURDIR\ops\k8s\local\nginx-configmap.yaml
 kubectl apply -f $CURDIR\ops\k8s\local\nginx-deploy.yaml
 kubectl apply -f $CURDIR\ops\k8s\local\nginx-service.yaml
