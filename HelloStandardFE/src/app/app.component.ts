85866322ea7f577476c801ebbccbda1790227471
loganballard <loganballard@gmail.com>
2020-08-07 11:24:09 -0700

    removing non used import

--- HelloStandardFE/src/app/app.component.ts
+++ HelloStandardFE/src/app/app.component.ts
@@ -1,7 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import { environment } from '../environments/environment';
-import { env } from 'process';
 
 @Component({
   selector: 'app-root',
