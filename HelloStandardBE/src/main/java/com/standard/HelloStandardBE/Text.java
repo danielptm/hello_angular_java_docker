edcf735169e31624c3973b777cf9528c9a5d3058
loganballard <loganballard@gmail.com>
2020-10-25 14:14:35 -0700

    updating dockerfile

--- HelloStandardBE/src/main/java/com/standard/HelloStandardBE/Text.java
+++ HelloStandardBE/src/main/java/com/standard/HelloStandardBE/Text.java
@@ -15,7 +15,7 @@ public class Text {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public Response get() {
-        String[] items = new String[]{"Hello Standard", "Hello from springboot", "Hello from docker", "Hello from java", "Hello angular"};
+        String[] items = new String[]{"hjvjhbuyyuif67fiuyg", "Hello Standard", "hell others", "Hello from springboot", "Hello from docker", "Hello from java", "Hello angular"};
         int i = (int) Math.floor(Math.random() * 5);
         String group = items[i];
         System.out.println("Fetched target group: " + group);
