9105dbb5722a1a57ad17f04c897351a215f3ed23
Daniel <danielptm@me.com>
2022-06-30 10:29:45 -0700

    retrigger

--- HelloStandardBE/src/main/java/com/standard/HelloStandardBE/Text.java
+++ HelloStandardBE/src/main/java/com/standard/HelloStandardBE/Text.java
@@ -1,5 +1,11 @@
 package com.standard.HelloStandardBE;
 
+import com.azure.core.http.rest.PagedIterable;
+import com.azure.storage.blob.BlobClient;
+import com.azure.storage.blob.BlobContainerClient;
+import com.azure.storage.blob.BlobServiceClient;
+import com.azure.storage.blob.BlobServiceClientBuilder;
+import com.azure.storage.blob.models.BlobItem;
 import org.springframework.stereotype.Component;
 
 import javax.ws.rs.GET;
@@ -7,6 +13,9 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 @Path("text")
@@ -20,5 +29,41 @@ public class Text {
         String group = items[i];
         System.out.println("Fetched target group: " + group);
         return Response.ok().entity(group).build();
+    }
+
+    @GET
+    @Path("pics")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getPics() {
+        BlobServiceClient blobServiceClient = new BlobServiceClientBuilder()
+                .connectionString("Connection string goes here")
+                .buildClient();
+
+        BlobContainerClient bcc = blobServiceClient.getBlobContainerClient("weddingceremony");
+
+        PagedIterable<BlobItem> pi = bcc.listBlobs();
+        List<Pic> items = new ArrayList<>();
+        for (BlobItem bi : pi.stream().collect(Collectors.toList())) {
+            String n = bi.getName();
+            BlobClient bc = bcc.getBlobClient(n);
+            Pic pic = new Pic(bc.getBlobUrl());
+            items.add(pic);
+        }
+        return Response.ok(items).build();
+    }
+}
+
+class Pic {
+    String path;
+    public Pic(String path) {
+        this.path = path;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
     }
 }
